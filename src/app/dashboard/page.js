a70810a104138cee2d4391c86dce75c6c9544087
Artur-Santana <artursantana150@gmail.com>
2026-03-23 20:41:29 -0300

    feat: :sparkles: add edificios CRUD with Supabase

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -7,8 +7,11 @@ import { useRouter } from "next/navigation";
 
 export default function Dashboard() {
   const [usuario, setUsuario] = useState(null)
+  const [edificios, setEdificios] = useState([])
+  const [nome, setNome] = useState("")
+  const [endereco, setEndereco] = useState("")
   const router = useRouter()
- 
+  
   useEffect(() => {
     async function verificarSessao() {
       const { data } = await supabase.auth.getUser()
@@ -20,15 +23,41 @@ export default function Dashboard() {
     }
     verificarSessao()
   }, [])
-  async function handleLogout() {
-    await supabase.auth.signOut()
-    router.push("/login")
+  async function getEdificios() {
+    const { data } = await supabase.from("edificios").select("*")
+    if (data) setEdificios(data)
   }
+  
+  useEffect(() => {
+    getEdificios()
+  }, [])
+  async function insertEdificio(e) {
+    e.preventDefault()
+    const { error } = await supabase.from("edificios").insert({ nome, endereco })
+    if (!error) {
+      getEdificios() // re-busca a lista
+    }
+  }
+     
+    async function handleLogout() {
+      await supabase.auth.signOut()
+      router.push("/login")
+    }
 
   return (
     <main>
+      <form onSubmit={insertEdificio}>
+        <input value={nome}
+            onChange={(e)=> setNome(e.target.value)} type="text" placeholder="Nome do edificio" />
+        <input value={endereco}
+             onChange={(e)=> setEndereco(e.target.value)} type="text" placeholder="Endereço" />
+        <button type="submit">Enviar</button>
+      </form>
       <h1>Pagina de dashboard!</h1>
       <p>Olá {usuario?.email}</p>
+      {edificios.map(edificio => (
+      <p key={edificio.id}>{edificio.nome}</p>
+      ))}
       <button onClick={handleLogout}>Sair</button>
     </main>
   )
