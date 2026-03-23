30cfeb1d5890b256624f91c9155eba174f0daa8c
Artur-Santana <artursantana150@gmail.com>
2026-03-23 20:07:53 -0300

    feat: :sparkles: protect dashboard route and add logout

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,11 +1,35 @@
+"use client"
 import supabase from "@/lib/supabase";
+import { useEffect } from "react"
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 
-export default async function Home() {
+export default function Dashboard() {
+  const [usuario, setUsuario] = useState(null)
+  const router = useRouter()
+ 
+  useEffect(() => {
+    async function verificarSessao() {
+      const { data } = await supabase.auth.getUser()
+      if (!data.user) {
+        router.push("/login")
+      }else{
+        setUsuario(data.user)
+      }
+    }
+    verificarSessao()
+  }, [])
+  async function handleLogout() {
+    await supabase.auth.signOut()
+    router.push("/login")
+  }
 
   return (
     <main>
       <h1>Pagina de dashboard!</h1>
+      <p>Olá {usuario?.email}</p>
+      <button onClick={handleLogout}>Sair</button>
     </main>
   )
 }
